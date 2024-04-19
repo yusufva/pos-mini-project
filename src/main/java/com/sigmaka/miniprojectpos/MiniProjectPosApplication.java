a3f505ed8b8a95f5cdb764b2e2ffa776b9937afb
yusufva <yusufvalent@gmail.com>
2024-04-19 15:01:40 +0700

    initial commit

---
+++ src/main/java/com/sigmaka/miniprojectpos/MiniProjectPosApplication.java
@@ -0,0 +1,13 @@
+package com.sigmaka.miniprojectpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectPosApplication.class, args);
+    }
+
+}
